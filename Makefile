449bfaa5ceb5d6523bae48613c258f05fc5ff372
Adrien Luitot <23219818+adrienluitot@users.noreply.github.com>
2023-12-07 21:36:06 +0100

    more universal Makefile + first real tests

--- Makefile
+++ Makefile
@@ -1,12 +1,27 @@
+SRC_DIRS = Dot Simulator Tests # Json
+SRCS = $(shell find $(SRC_DIRS) -name "*.cpp")
 
-output/Test.o:
-	g++ -c Tests/Test.cpp -o output/Test.o
+DOT_OBJS = $(addprefix output/, SchematicObject.o Token.o Dot.o)
 
-output/test_dot: output/Test.o
-	g++ -c Tests/test_dot.cpp -o output/test_dot.o
 
-tests: output/test_dot
-	g++ output/Test.o output/test_dot.o -o output/test
+output/%.o: Dot/%.cpp
+	@mkdir -p output
+	g++ -c $< -o $@
+
+output/%.o: Json/%.cpp
+	@mkdir -p output
+	g++ -c $< -o $@
+
+output/%.o: Simulator/%.cpp
+	@mkdir -p output
+	g++ -c $< -o $@
+
+output/%.o: Tests/%.cpp
+	@mkdir -p output
+	g++ -c $< -o $@
+
+tests: output/test_dot.o output/Test.o $(DOT_OBJS)
+	g++ $? -o output/test_dot 
 
 clean:
 	rm -rf output/*
--- Tests/test_dot.cpp
+++ Tests/test_dot.cpp
@@ -1,5 +1,6 @@
 #include "Test.hpp"
 #include "../Dot/Dot.hpp"
+#include "../Dot/Token.hpp"
 
 using namespace std;
 
@@ -7,9 +8,25 @@ int main() {
 
     Dot dotParser("database/dot/test1.dot");
 
+
+    // === Token Function ===
+    Test TC_Token("Token Functions");
+        //  == `checkFirstToken` ==
+        // Setup
+        Token firstToken("label", "keyword", NULL, NULL, 2, 10);
+        Token secondToken("label", "keyword", &firstToken, NULL, 2, 10);
+        firstToken.setNextToken(&secondToken);
+        
+        // Test
+        TC_Token.check(firstToken.isFirst());
+        TC_Token.check(!secondToken.isFirst());
+
+
+
+
     Test dotLexer("Dot Lexer"); // create test case for Lexer
-    // run the lexer for the test dot (0 means error while lexing)
-    dotLexer.check(!dotParser.launchLexer());
+    // // run the lexer for the test dot (0 means error while lexing)
+    // dotLexer.check(!dotParser.launchLexer());
     
     
     
