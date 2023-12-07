083c85af7280e74e5396cee960fc09440181d599
arthuuuuroo <talarmainarthur@gmail.com>
2023-12-07 18:08:41 +0100

    check + debut lexer ajout des check de type de token et de firstToken, + debut du code du lexer

--- Dot/Dot.cpp
+++ Dot/Dot.cpp
@@ -1,6 +1,95 @@
 #include "Dot.hpp"
 
+ string Dot::specialCharacter = ">/\\[]=";
+ string Dot::anyWordStarter = "\"";
+ string Dot::keyword[3] = ["label", "sel", "digraph"];
+ string Dot::forbidennCharacter; = "\'`";
+
 Dot::Dot(string file_path):
-file_path(file_path) {
+file_path(file_path), first_token(*NULL) {
+    
+}
+
+Dot::get_filePath(){
+    return this->file_path;
+}
+
+Dot::get_firstToken(){
+    return this->first_token;
+}
+
+Dot::lexer(){
+    ifstream input_file(this->get_filePath());
+    char c;
+    string buffer;
+    unsigned int line_number = 0;
+    unsigned int column_number = 0;
+    Token *previousToken;
+    Token * currentToken;
+
+    if (!input_file){
+        return 1;
+
+    } else {
+
+        while(getline(input_file, line)){
+            column_number = 0;
+            while(line[column_number] != 0){
+
+                if (checkType(c, specialCharacter)){
+
+                    if (checkFirstToken()){
+                        currentToken = new Token firstToken(str(c), "specialCharacter", *NULL, *NULL, line_number, column_number);
+                    } else {
+                        currentToken = new Token (str(c), "specialCharacter", &previousToken, *NULL, line_number, column_number);
+                    }
+                    i++;
+                } else if(checkType(buffer, keyword[])){
+                    if (checkFirstToken()){
+                        currentToken = new Token firstToken(buffer, "anyWord", *NULL, *NULL, line_number, column_number);
+                    } else {
+                        currentToken = new Token (buffer, "anyWord", &previousToken, *NULL, line_number, column_number);
+                    }
+                    i++;
+                } else {
+                    if (c == '"'){
+                        
+                    }
+                }
+                *previousToken = &currentToken;
+                }
+            }
+            column_number++;
+        }
+        line_number++;
+    }
 
 }
+
+Dot::checkType(char c, string specialCharacter){
+    if specialCharacer.find(c) != npos {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Dot::checkType(string keyword, string keyword[]){ /// vérifier pour la tailledu tableau 
+    for(int i=0; i<=3; i++){
+        if (!(strcmp(keyword, keyword[i]))){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool checkFirstToken(Token *token){
+    if (*token->getPreviousToken() == *NULL){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
--- Dot/Dot.hpp
+++ Dot/Dot.hpp
@@ -2,6 +2,7 @@
 #define _DOT_H_
 
 #include "Token.hpp"
+#include <fstream>
 #include <string>
 
 using namespace std;
@@ -12,6 +13,15 @@ class Dot {
         Token* first_token;
 
     public:
+        // ======= TOKEN CONTAINER ========
+
+        //string anyWordLetter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
+        static string specialCharacter;
+        static string anyWordStarter;
+        static string keyword[];
+        static string forbiddenCharacter;
+
+
         // =======  CONSTRUCTOR / DESTRUCTOR =======
         Dot(string file_path);
         ~Dot();
@@ -19,6 +29,15 @@ class Dot {
 
 
         // =======  GETTERS / SETTERS =======
+        string get_filePath();
+        Token* get_firstToken();
+
+        // =======  OTHER FUNCTIONS =======
+        int lexer(void);
+        bool checkType(char c, string container);
+        bool checkType(string word, string keyword[]);
+        bool checkFirstToken(Token *token);
+
 
         /** @brief Run the lexer, and initiate first_token
          * @return unsigned int: status code **/
