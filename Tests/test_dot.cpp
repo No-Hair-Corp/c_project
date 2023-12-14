e729200e69d0902257998e4d04722babc832cca0
Raph <raphael@LAPTOP-KKCFHHLI.localdomain>
2023-12-14 17:13:00 +0100

    Tests DOT tests de quand ca doit marcher ET quand ca doit pas marcher + fonction jumpComments + database tests lexer

--- Tests/test_dot.cpp
+++ Tests/test_dot.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
 
     // === Token Functions ===
-    Test TC_Token("Token Functions");
+    Test TC_Token("Token Functions"); // create test case for Token Functions 
         //  == checkFirstToken ==
         // Setup
         Token firstToken("label", "keyword", NULL, NULL, 2, 10);
@@ -20,7 +20,7 @@ int main() {
 
     // === Dot Lexer ===
     // === Check Functions ===
-    Test TC_Check("Check Functions"); // create test case for Check functions //TODO: fonctions a passer en static ???
+    Test TC_Check("Check Functions"); //TODO: fonctions a passer en static et en bool
         // == checkType checkKeywords CheckArrow ==
         // Setup
         Dot file("/database/Dot/test_and2_true");
@@ -40,6 +40,7 @@ int main() {
         TC_Check.check(file.checkType(c1, Dot::specialCharacter));
         TC_Check.check(file.checkType(c2, Dot::stringStarter));
         TC_Check.check(file.checkType(c3, Dot::forbiddenCharacter));
+        TC_Check.check(!file.checkType(c1, Dot::forbiddenCharacter));
         TC_Check.check(file.checkKeywords(keyword1,5));
         TC_Check.check(!file.checkKeywords(keyword2,5));
         TC_Check.check(file.CheckArrow(one,1));
@@ -47,11 +48,12 @@ int main() {
         TC_Check.check(!file.CheckArrow(three,1));
     
     // === Register Functions ===
-    Test TC_Register("Register Functions"); //TODO: passer fonctions en static
+    Test TC_Register("Register Functions"); //TODO: passer fonctions en static et en bool
         // == registerString ==
         // Setup
         ifstream fileString1("/database/Dot/test1_registerString");
         ifstream fileString2("/database/Dot/test2_registerString");
+        ifstream fileString3("/database/Dot/test3_registerString");
 
         string line;
         string buffer="";
@@ -61,21 +63,44 @@ int main() {
         // Test
         TC_Register.check(file.registerString(fileString1,line,column_number,line_number,buffer));
         TC_Register.check(file.registerString(fileString2,line,column_number,line_number,buffer));
+        TC_Register.check(!file.registerString(fileString3,line,column_number,line_number,buffer));
 
         // == registerKeywords ==
         // Setup
-        ifstream fileKeyword("/database/Dot/test_registerKeywords");
+        ifstream fileKeyword1("/database/Dot/test1_registerKeywords");
+        ifstream fileKeyword2("/database/Dot/test1_registerKeywords");
 
         buffer="";
         line_number = 0;
         column_number = 0;
 
         // Test
-        TC_Register.check(file.registerString(fileKeyword,line,column_number,line_number,buffer));
+        TC_Register.check(file.registerString(fileKeyword1,line,column_number,line_number,buffer));
+        TC_Register.check(!file.registerString(fileKeyword2,line,column_number,line_number,buffer));
     
+    // === jumpComments Function ===
+    Test TC_Jump_Comments("Jump Comments Function"); //passer fonction en static et en bool
+        // == jumpComments ==
+        // Setup
+        ifstream fileComment1("/database/Dot/test1_jumpComments");
+        ifstream fileComment2("/database/Dot/test2_jumpComments");
+        ifstream fileComment3("/database/Dot/test3_jumpComments");
+        ifstream fileComment4("/database/Dot/test4_jumpComments");
+        ifstream fileComment5("/database/Dot/test5_jumpComments");
+        ifstream fileComment6("/database/Dot/test6_jumpComments");
+
+        // Test
+        TC_Jump_Comments.check(file.registerString(fileComment1,line,column_number,line_number,buffer));
+        TC_Jump_Comments.check(file.registerString(fileComment2,line,column_number,line_number,buffer));
+        TC_Jump_Comments.check(file.registerString(fileComment3,line,column_number,line_number,buffer));
+        TC_Jump_Comments.check(!file.registerString(fileComment4,line,column_number,line_number,buffer));
+        TC_Jump_Comments.check(!file.registerString(fileComment5,line,column_number,line_number,buffer));
+        TC_Jump_Comments.check(!file.registerString(fileComment6,line,column_number,line_number,buffer));
+
+
     // === Lexer Function ===
-    Test TC_Lexer("Lexer Function");
-        // == Lexer ==
+    Test TC_Lexer("Lexer Function"); //passer fonction en static et en bool
+        // == lexer ==
         // Setup
         Dot file1("/database/Dot/test_and2_true");
         Dot file2("/database/Dot/test_and2_correct");
@@ -85,6 +110,8 @@ int main() {
         TC_Lexer.check(file1.lexer());
         TC_Lexer.check(file2.lexer());
         TC_Lexer.check(file3.lexer());
+
+        //TODO afficher token et comparer avec fichier entrée pour voir si ça lexe bien
 
     return 0;
 }
